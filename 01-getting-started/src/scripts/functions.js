b17cb7823f7b5b2dc773f2f8de39cdd7623a5032
sethmccauley <sam3353@gmail.com>
2020-02-21 12:13:16 -0700

    Changed tests and size functionality.

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -2,6 +2,7 @@
 const functions = {
     
     size: (num) => {
+        if (num < -100) return "extra large";
         if (num < 10) return "small";
         if (num < 20) return "medium";
         return "large";
