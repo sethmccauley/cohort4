072732825841e404d54f199e11f2e0c17e93d9af
sethmccauley <sam3353@gmail.com>
2020-02-27 09:47:06 -0700

    Refined comments.

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -59,7 +59,7 @@ const functions = {
             }
             income -= e["bracket"];
         })
-        //Fix Truncation to two decimal places
+        //Fix Truncation to two decimal places without using Math.floor or round()
         totalTax = totalTax.toString().match(/^\d+(?:\.\d{0,2})?/)[0];
         return parseFloat(totalTax, 10);
     },
