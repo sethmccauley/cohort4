98bf8270b1aadd4d677fa27aaeb822f1be287694
sethmccauley <sam3353@gmail.com>
2020-03-02 08:51:45 -0700

    Adjusted/Added tests for list manipulation.

--- 02-dom/src/scripts/functions.test.js
+++ 02-dom/src/scripts/functions.test.js
@@ -1,18 +1,5 @@
 import functions from './functions.js'
 
-test('Count child elements of target element.', () => {
-    document.body.innerHTML = `
-        <ol id="orderedList">
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ol>
-    `;
-
-    const orderedList = document.getElementById('orderedList');
-    expect(orderedList.children.length).toBe(3);
-});
-
 test('Append list item to ordered list.', () => {
     document.body.innerHTML = `
         <ol id="orderedList">
