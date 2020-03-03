206c23a6ac4b93b118be7c5e0fab88189a502068
sethmccauley <sam3353@gmail.com>
2020-03-03 10:30:08 -0700

    Added test.

--- 01-getting-started/javascript/daily.test.js
+++ 01-getting-started/javascript/daily.test.js
@@ -4,4 +4,5 @@ test('Check the conversion.', () => {
     expect(functions.convertToFahrenheit(-40)).toBe(-40);
     expect(functions.convertToFahrenheit(0)).toBe(32);
     expect(functions.convertToFahrenheit(28)).toBe(82);
+    expect(functions.convertToFahrenheit(100)).toBe(212);
 });
