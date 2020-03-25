4c9ab478d68884b315f16e3b3ebcf8d22cd2fd86
Seth McCauley <seth.a.mccauley@gmail.com>
2020-03-25 11:12:18 -0600

    Adding components to project.

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.counter = 13;
+    this.state = {
+      myState: "TBD"
+    }
+  }
+  onPushMe = () => {
+    this.counter += 1;
+    console.log(this.counter);
+    this.setState({
+      myState: 'now: ' + this.counter
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in control of this application and my name is Seth. {this.state.myState} </h1>
+          <button onClick={this.onPushMe}>
+            Push Me
+          </button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <MyComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-01/src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyComp extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Hello World from my component.</h1>
+            </div>
+        )
+    }
+}
+
+export default MyComp;
