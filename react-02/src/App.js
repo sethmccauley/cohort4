a7cbece4e2c1ebe34858be0345c2510de1f371f7
Seth McCauley <seth.a.mccauley@gmail.com>
2020-03-26 20:19:00 -0600

    Created SVG components and events.

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import Cloud from './components/cloud.js';
+import SnesSVG from './components/snes.js';
+import PsSVG from './components/ps.js';
+import XboxSVG from './components/xbox.js';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  clickTest = () => {
+    console.log('SVG Was clicked!');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div style={{backgroundColor: '#282c34'}}>
+          <br />
+          <Cloud height="100px" width="100px" className="App-svg" onClick={this.clickTest} />
+          <SnesSVG height="100px" width="100px" className="App-svg" onClick={this.clickTest} />
+          <PsSVG height="100px" width="100px" className="App-svg" onClick={this.clickTest} />
+          <XboxSVG height="100px" width="100px" className="App-svg" onClick={this.clickTest} />
+        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo-anti" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
