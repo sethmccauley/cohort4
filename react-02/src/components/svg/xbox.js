17f4ec5c2863c16be9f8031a8ce5d4037fde0902
Seth McCauley <seth.a.mccauley@gmail.com>
2020-04-01 11:22:03 -0600

    Fixed props reference.

--- react-02/src/components/svg/xbox.js
+++ react-02/src/components/svg/xbox.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class XboxSVG extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
     clickChange = () => {
