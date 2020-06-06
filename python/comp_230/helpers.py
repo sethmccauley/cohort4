9f6bf7a6e88b772e0d7aabd9372e9453843d25f3
Seth McCauley <seth.a.mccauley@gmail.com>
2020-06-05 20:47:56 -0600

    InnerJoin finished.

--- python/comp_230/helpers.py
+++ python/comp_230/helpers.py
@@ -10,7 +10,7 @@ def breakIntoDict(file):
     for sheet in wb.sheetnames:
         builtDict[sheet] = {}
         rowCount = 1
-        for row in wb[sheet].iter_rows(min_row=1,min_col=1):
+        for row in wb[sheet].iter_rows(min_row=2,min_col=1):
             if row[0].value is not None:
                 col = 0
                 builtDict[sheet][rowCount] = {}
@@ -20,12 +20,38 @@ def breakIntoDict(file):
             rowCount += 1
     return builtDict
 
-def innerJoin(dict_one, dict_two, join_on):
-    return False
+# def innerJoin(dict_one, dict_two, column, value=None):
+#     resultingDict = {}
+#     index = 1
+#     for dOne_rows in dict_one.values():
+#         records = 1
+#         tempHold = {}
+#         for dTwo_rows in dict_two.values():
+#             if dOne_rows[column] == dTwo_rows[column]:
+#                 tempHold[records] = dTwo_rows
+#                 records += 1
+#         combined = {}
+#         combined.update(dOne_rows)
+#         combined.update(tempHold)
+#         resultingDict[index] = combined
+#         index += 1
+#     return resultingDict
 
-test = breakIntoDict(localFile)
-print(test['customers'])
-print('')
-print(test['invoices'])
-print('')
-print(test['invoice_items'])
+def innerJoin(dict_one, dict_two, column, lookup_value=None):
+    resultingDict = {}
+    index = 1
+    # Construct smaller tables to iterate through later if passed a lookup value
+    if lookup_value != None:
+        dict_one = {key:value for key,value in dict_one.items() if value[column] == lookup_value}
+        dict_two = {key:value for key,value in dict_two.items() if value[column] == lookup_value}
+
+    for dOne_rows in dict_one.values():
+        for dTwo_rows in dict_two.values():
+            if dOne_rows[column] == dTwo_rows[column]:
+                combined = {}
+                combined.update(dOne_rows)
+                combined.update(dTwo_rows)
+                resultingDict[index] = combined
+                index += 1
+
+    return resultingDict
