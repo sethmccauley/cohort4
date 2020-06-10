b0fb7b10901e000fefbf906424bfb1d0681f8311
Seth McCauley <seth.a.mccauley@gmail.com>
2020-06-10 08:53:54 -0600

    Small syntax updates.

--- 03-objects/src/scripts/main.js
+++ 03-objects/src/scripts/main.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 addCity.addEventListener('click', async () => {
     cityController.createCity(document.getElementById('cityName').value,document.getElementById('cityLat').value,document.getElementById('cityLong').value,document.getElementById('cityPop').value,keyNumber);
-    let confirmation = await functions.postData('http://localhost:5000/add',{key: keyNumber, city: cityController.cityList[cityController.cityList.length - 1]});
+    let confirmation = await functions.postData('http://localhost:5000/add',{"key": keyNumber, "city": cityController.cityList[cityController.cityList.length - 1]});
     if (confirmation.status == 200) {
         console.log('City Added to Remote.');
         keyNumber++;
